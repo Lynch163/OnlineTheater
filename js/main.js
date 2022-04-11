5bdc2e778e6c9ea6fd9eb0acc79bcd3e8b41990d
Lynch163 <92573729+Lynch163@users.noreply.github.com>
2022-04-11 23:37:38 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-function x() {
-return 'hello! My name is Aleksandr'
-}
+const modalWindow = document.querySelector('.modal');
+const buttonsModal = document.querySelectorAll('.button-play');
+
+buttonsModal.forEach((item, i) => {
+item.addEventListener('click', () => {
+modalWindow.classList.add('active');
+});
+});
+
+modalWindow.addEventListener('click', () => {
+modalWindow.classList.remove('active');
+});
